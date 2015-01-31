cfec0f6308f1e9c4fb27f601f4845aad6af14967
sevak gharibian <sevak.gharibian@gmail.com>
2015-01-31 15:48:11 +0330

    initial version, included MCI/Jiring

---
+++ src/main/java/com/infotech/isg/proxy/HashGenerator.java
@@ -0,0 +1,31 @@
+package com.infotech.isg.proxy;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * utility class for generating hash
+ *
+ * @author Sevak Gharibian
+ */
+public class HashGenerator {
+
+    public static String getMD5(String input) {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5 digest error", e);
+        }
+        byte[] hash = md.digest(input.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (Byte b : hash) {
+            String hex = Integer.toHexString(b & 0xff);
+            if (hex.length() == 1) {
+                sb.append("0");
+            }
+            sb.append(hex);
+        }
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/infotech/isg/proxy/jiring/JiringProxyImpl.java
@@ -0,0 +1,57 @@
+package com.infotech.isg.proxy.jiring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * implementation of proxy client for Jiring service.
+ *
+ * @author Sevak Gharibian
+ */
+public class JiringProxyImpl implements JiringProxy {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JiringProxyImpl.class);
+
+    private static final String SALES_REQUEST_FUNCTION_NAME = "SALESREQUEST";
+    private static final String SALES_REQUEST_EXEC_FUNCTION_NAME = "SALESREQUESTEXEC";
+    private static final String SALES_REQUEST_EXEC_PARAM_2 = "APPROVE";
+
+    private final String url;
+    private final String username;
+    private final String password;
+    private final String brand;
+
+    public JiringProxyImpl(String url, String username, String password, String brand) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+        this.brand = brand;
+    }
+
+    @Override
+    public TCSResponse salesRequest(String consumer, int amount) {
+        TCSRequest request = new TCSRequest();
+        request.setUsername(username);
+        request.setPassword(password);
+        request.setFunctionName(SALES_REQUEST_FUNCTION_NAME);
+        request.setFunctionParam1(brand);
+        request.setFunctionParam2(Integer.toString(amount));
+        request.setFunctionParam4(username);
+        request.setFunctionParam6(consumer);
+        request.setFunctionParam7(consumer);
+        TCSResponse response = TCSConnection.call(request, url);
+        return response;
+    }
+
+    @Override
+    public TCSResponse salesRequestExec(String token) {
+        TCSRequest request = new TCSRequest();
+        request.setUsername(username);
+        request.setPassword(password);
+        request.setFunctionName(SALES_REQUEST_EXEC_FUNCTION_NAME);
+        request.setFunctionParam1(token);
+        request.setFunctionParam2(SALES_REQUEST_EXEC_PARAM_2);
+        TCSResponse response = TCSConnection.call(request, url);
+        return response;
+    }
+}
